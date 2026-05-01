0c2b9ba8b32ac742794e1c8dfe7213c8e6479704
Dai Thang <23521422@gm.uit.edu.vn>
2026-05-01 23:58:28 +0700

    ci: add NVD API key handling to dependency check script

--- vars/dependencyCheck.groovy
+++ vars/dependencyCheck.groovy
@@ -1,26 +1,30 @@
 def call() {
     echo '>>> Running SCA Scan...'
+
     sh '''
         mkdir -p dependency-check-report
         mkdir -p /var/jenkins_home/dependency-check-data
     '''
 
     catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
-        sh """
-            docker run --rm \\
-                -v ${env.WORKSPACE}:/src \\
-                -v ${env.WORKSPACE}/dependency-check-report:/report \\
-                -v /var/jenkins_home/dependency-check-data:/usr/share/dependency-check/data \\
-                owasp/dependency-check:latest \\
-                --project "DocVault" \\
-                --scan /src \\
-                --exclude "**/node_modules/**" \\
-                --exclude "**/.pnpm-store/**" \\
-                --format "HTML" \\
-                --format "JSON" \\
-                --out /report \\
-                --failOnCVSS 7 \\
-                --disableKnownExploited
-        """
+        withCredentials([string(credentialsId: 'nvd-api-key', variable: 'NVD_API_KEY')]) {
+            sh """
+                docker run --rm \\
+                    -v ${env.WORKSPACE}:/src \\
+                    -v ${env.WORKSPACE}/dependency-check-report:/report \\
+                    -v /var/jenkins_home/dependency-check-data:/usr/share/dependency-check/data \\
+                    owasp/dependency-check:latest \\
+                    --project "DocVault" \\
+                    --scan /src \\
+                    --exclude "**/node_modules/**" \\
+                    --exclude "**/.pnpm-store/**" \\
+                    --format "HTML" \\
+                    --format "JSON" \\
+                    --out /report \\
+                    --failOnCVSS 7 \\
+                    --disableKnownExploited \\
+                    --nvdApiKey "$NVD_API_KEY"
+            """
+        }
     }
 }
