6ca6f575219ad67023ae6d5e3535e16725328281
Dai Thang <23521422@gm.uit.edu.vn>
2026-05-09 21:04:18 +0700

    feat(security): enhance Trivy filesystem scan by excluding additional directories and improving cleanup process

--- vars/trivyFsScan.groovy
+++ vars/trivyFsScan.groovy
@@ -2,9 +2,41 @@ def call(cfg) {
     echo '>>> Running Trivy Filesystem Scan...'
     echo '>>> Security gate policy: Trivy FS fails on HIGH/CRITICAL findings.'
     sh """
+        set -eu
+
+        scan_dir="\$(mktemp -d)"
+        cleanup() {
+            rm -rf "\$scan_dir"
+        }
+        trap cleanup EXIT
+
+        tar \\
+            --exclude='.git' \\
+            --exclude='*/.git' \\
+            --exclude='node_modules' \\
+            --exclude='*/node_modules' \\
+            --exclude='*/node_modules/*' \\
+            --exclude='.pnpm-store' \\
+            --exclude='*/.pnpm-store' \\
+            --exclude='*/.pnpm-store/*' \\
+            --exclude='.turbo' \\
+            --exclude='*/.turbo' \\
+            --exclude='.next' \\
+            --exclude='*/.next' \\
+            --exclude='dist' \\
+            --exclude='*/dist' \\
+            --exclude='coverage' \\
+            --exclude='*/coverage' \\
+            --exclude='dependency-check-report' \\
+            --exclude='checkov-report' \\
+            --exclude='zap-report' \\
+            --exclude='*/.terraform' \\
+            --exclude='*/.terraform/*' \\
+            -cf - -C '${env.WORKSPACE}' . | tar -xf - -C "\$scan_dir"
+
         docker run --rm \\
-            -v ${env.WORKSPACE}:/src \\
+            -v "\$scan_dir:/src:ro" \\
             ${cfg.trivyImage} \\
-            fs /src --scanners vuln,secret,misconfig --misconfig-scanners dockerfile,kubernetes,helm --severity HIGH,CRITICAL --exit-code 1 --no-progress --skip-dirs .pnpm-store,node_modules,.terraform
+            fs /src --scanners vuln,secret,misconfig --misconfig-scanners dockerfile,kubernetes,helm --severity HIGH,CRITICAL --exit-code 1 --no-progress
     """
 }
